e516b69454d0cec5a00a7483b10c9f9639097e99
Misha-testeststs <66586926+Misha-testeststs@users.noreply.github.com>
2020-07-01 19:22:26 +0900

    Create VPC.tf
    create VPC.tf

---
+++ VPC.tf
@@ -0,0 +1,13 @@
+variable "test-vpc-cidr" {
+  description = "CIDR for the whole VPC"
+  default     = "10.0.0.0/16"
+}
+
+resource "aws_vpc" "test-vpc" {
+  cidr_block           = "${var.test-vpc-cidr}"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "TEST-VPC"
+  }
+}
